dd5abc395682d381f6d29ede2f1446f1f51fff57
Edrei <edreifernandes@gmail.com>
2020-10-23 23:40:21 -0300

    Criando panel de informações com tabela de usuario e lista de espera

--- src/app/AppLayout.java
+++ src/app/AppLayout.java
@@ -5,7 +5,10 @@ import java.awt.Dimension;
 import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class AppLayout extends JFrame {
 
@@ -13,10 +16,16 @@ public class AppLayout extends JFrame {
     private final int width = 600;
     private final int border = 10;
 
+    private JTable usersTable;
+    private JList<String> waitList;
+
     public AppLayout() {
         JPanel panelNorth = new JPanel(new BorderLayout());
-        borderedPanel(panelNorth, height / 5, width);
+        JPanel panelInfo = borderedPanel(panelNorth, height / 5, width);
         add(panelNorth, BorderLayout.NORTH);
+
+        instantiateUsersTable(panelInfo);
+        instantiateWaitList(panelInfo);
 
         JPanel panelCenter = new JPanel(new BorderLayout());
         borderedPanel(panelCenter, height / 5, width);
@@ -29,7 +38,29 @@ public class AppLayout extends JFrame {
         instantiateFrame();
     }
 
-    private void borderedPanel(JPanel _panel, int _height, int _width) {
+    private void instantiateUsersTable(JPanel _panel) {
+        String[] columnsNames = { "User", "Status", "Address" };
+
+        Object[][] data = { { "User1", "Conectado", "4240" }, { "User2", "Conectado", "4241" },
+                { "User3", "Conectado", "4242" }, { "User4", "Conectado", "4243" } };
+
+        usersTable = new JTable(data, columnsNames);
+        usersTable.setPreferredScrollableViewportSize(new Dimension(500, 50));
+        usersTable.setFillsViewportHeight(true);
+
+        _panel.add(new JScrollPane(usersTable), BorderLayout.WEST);
+    }
+
+    private void instantiateWaitList(JPanel _panel) {
+        String[] listItens = { "item 1", "item 2", "item 3", "item 4" };
+        waitList = new JList<String>(listItens);
+        waitList.setVisibleRowCount(5);
+
+        waitList.setPreferredSize(new Dimension(100, 50));
+        _panel.add(new JScrollPane(waitList), BorderLayout.EAST);
+    }
+
+    private JPanel borderedPanel(JPanel _panel, int _height, int _width) {
         _panel.setBackground(Color.GRAY);
         _panel.setPreferredSize(new Dimension(_width, _height));
 
@@ -52,6 +83,11 @@ public class AppLayout extends JFrame {
         borderEast.setBackground(Color.BLACK);
         borderEast.setPreferredSize(new Dimension(border, _height));
         _panel.add(borderEast, BorderLayout.EAST);
+
+        JPanel panelCenter = new JPanel(new BorderLayout());
+        _panel.add(panelCenter);
+
+        return panelCenter;
     }
 
     private void instantiateFrame() {
